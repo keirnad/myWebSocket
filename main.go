3efd3b1c1b5ad08e8ef43374d54c2cce19c71520
Dzedz Timofej <dzedz@internet.ru>
2026-06-29 20:03:15 +0300

    Add handleConnections function

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package websocket
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -8,4 +11,26 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
+	for {
+		messageType, message, err := ws.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		log.Printf("Recieved: %s", message)
+
+		if err := ws.WriteMessage(messageType, message); err != nil {
+			log.Println(err)
+			break
+		}
+	}
 }
